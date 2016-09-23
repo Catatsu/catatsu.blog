ea0230ae83c40a7c30d7fa28fa492dcd84a6f8d8
nabeen <watanabe_kenichiro@hasigo.co.jp>
2016-09-23 21:53:53 +0900

    test commit

---
+++ content/post/2016-09-23-hello-world.md
@@ -0,0 +1,11 @@
++++
+date = "2016-09-23T20:22:41+09:00"
+draft = true
+title = "Catatsuの技術ブログ始めました！"
+slug = "initial commit"
+author = "nabeen"
+categories = ["information"]
+
++++
+
+投稿テスト
---
+++ create.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -u
+
+usage() {
+  echo "Usage : $0 [-w] [title_name]" 1>&2
+}
+
+main () {
+  cd $(cd $(dirname $0) && pwd)
+  OPEN_EDITOR=0
+  for ARG; do
+    case "$ARG" in
+      -*)
+      while getopts w OPT "$ARG"; do
+        case "$OPT" in
+          w) OPEN_EDITOR=1;;
+          *) usage; exit 1;;
+        esac
+      done
+      ;;
+    esac
+  done
+
+  shift $(expr $OPTIND - 1)
+  [ $# -ne 1 ] && usage && exit 1
+
+  slug="$1"
+  file_name="post/$(date +"%Y-%m-%d")-${slug}.md"
+  file_path="content/$file_name"
+  if [ ! -e "$file_path" ]; then
+    hugo new "$file_name"
+    sed -i '' -e 's/title = \".*\"/title = \"\"/g' "$file_path"
+    sed -i '' -e "s/slug = \".*\"/slug = \"$slug\"/g" "$file_path"
+  else
+    echo "$file_name already exists."
+  fi
+
+  [ $OPEN_EDITOR -eq 1 ] && atom "$file_path"
+
+}
+main "$@"
